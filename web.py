b391d1409e1acbb3d194444d02cc14dbbf991c2b
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-15 15:16:29 -0300

    Interface and input box

--- web.py
+++ web.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import functions as f
 
-st.title("Todo List App")
-st.subheader("Welcome!")
+st.title('Todo List App')
+st.subheader('Welcome!')
+st.text('Start being productive right now!')
 todos = f.get_todos()
 
 # adds one checkbox for each to-do
 for todo in todos:
     st.checkbox(todo)
+
+st.text_input(label='', placeholder='Add a new todo...')
