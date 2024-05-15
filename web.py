b2d92aeb9312e78c4a2fd02a3afd567c5254a876
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-15 15:01:20 -0300

    Check box for each to-do

--- web.py
+++ web.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import functions as f
 
 st.title("Todo List App")
 st.subheader("Welcome!")
+todos = f.get_todos()
+
+# adds one checkbox for each to-do
+for todo in todos:
+    st.checkbox(todo)
