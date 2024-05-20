d4e98c8d3b8f164025c62be2fdcb97f3b7511c3c
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-20 15:00:40 -0300

    Add_todo() to update file according to new todos / User can now add todos / todos.txt untracked

--- web.py
+++ web.py
@@ -1,13 +1,29 @@
 import streamlit as st
 import functions as f
 
+todos = f.get_todos()   # gets to-dos from archive todos.txt
+
+
+def add_todo():
+    """
+    Updates todos file based on new inputs
+    """
+    added_todo = st.session_state['new_todo'] + '\n' # access widget's value according to its key (checkbox, input box, etc)
+    if added_todo:
+        added_todo = added_todo.capitalize()
+        todos.append(added_todo)
+        f.update_file(todos)
+
+
+# gui setup (header)
 st.title('Todo List App')
 st.subheader('Welcome!')
 st.text('Start being productive right now!')
-todos = f.get_todos()
 
 # adds one checkbox for each to-do
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    st.checkbox(todo, key=f'{index}')
 
-st.text_input(label='', placeholder='Add a new todo...')
+# on_change: action to be executed when user enters a prompt
+st.text_input(label='.', placeholder='Add a new todo...', key=f'new_todo', on_change=add_todo, label_visibility='hidden')
+print(st.session_state['new_todo'])
