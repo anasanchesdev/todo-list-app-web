ddb356b43d883a50d3f3bdff2b05f693d9a879aa
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-20 15:19:28 -0300

    Todos can be completed

--- web.py
+++ web.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import functions as f
 
-todos = f.get_todos()   # gets to-dos from archive todos.txt
+todos_list = f.get_todos()   # gets to-dos from archive todos.txt
 
 
 def add_todo():
     """
     Updates todos file based on new inputs
     """
-    added_todo = st.session_state['new_todo'] + '\n' # access widget's value according to its key (checkbox, input box, etc)
+    # access widget's value according to its key (checkbox, input box, etc)
+    added_todo = st.session_state['new_todo'] + '\n'
+
     if added_todo:
         added_todo = added_todo.capitalize()
-        todos.append(added_todo)
-        f.update_file(todos)
+        todos_list.append(added_todo)
+        f.update_file(todos_list)
 
 
 # gui setup (header)
@@ -21,9 +23,14 @@ st.subheader('Welcome!')
 st.text('Start being productive right now!')
 
 # adds one checkbox for each to-do
-for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f'{index}')
+for todo in todos_list:
+    st.checkbox(todo, key=todo)  # creates custom key for each to-do
+    if st.session_state[todo]:
+        todos_list.remove(todo)
+        f.update_file(todos_list)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 # on_change: action to be executed when user enters a prompt
 st.text_input(label='.', placeholder='Add a new todo...', key=f'new_todo', on_change=add_todo, label_visibility='hidden')
-print(st.session_state['new_todo'])
+st.session_state
