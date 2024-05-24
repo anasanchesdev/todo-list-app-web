d8137e453149e341d408ecfbd315169bd4fd7c1e
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-24 14:51:48 -0300

    Input box clears itself after new inputs gitignore added

--- web.py
+++ web.py
@@ -1,3 +1,7 @@
+"""
+TODO FIX: To-dos em branco sendo criados após completar o último to-do da lista.
+"""
+
 import streamlit as st
 import functions as f
 
@@ -6,7 +10,7 @@ todos_list = f.get_todos()   # gets to-dos from archive todos.txt
 
 def add_todo():
     """
-    Updates todos file based on new inputs
+    Updates todos.txt file based on new inputs
     """
     # access widget's value according to its key (checkbox, input box, etc)
     added_todo = st.session_state['new_todo'] + '\n'
@@ -15,7 +19,7 @@ def add_todo():
         added_todo = added_todo.capitalize()
         todos_list.append(added_todo)
         f.update_file(todos_list)
-
+        st.session_state['new_todo'] = ''  # resets input box
 
 # gui setup (header)
 st.title('Todo List App')
@@ -23,13 +27,14 @@ st.subheader('Welcome!')
 st.text('Start being productive right now!')
 
 # adds one checkbox for each to-do
-for todo in todos_list:
-    st.checkbox(todo, key=todo)  # creates custom key for each to-do
-    if st.session_state[todo]:
+for index, todo in enumerate(todos_list):
+    todo_key = f'{todo.strip()}_{index}'
+    st.checkbox(todo, key=todo_key)
+    if st.session_state[todo_key]:  # verifies if user has checked any to-do
         todos_list.remove(todo)
         f.update_file(todos_list)
-        del st.session_state[todo]
-        st.experimental_rerun()
+        del st.session_state[todo_key]  # deletes object from session_state
+        st.experimental_rerun()  # updates page with new data
 
 # on_change: action to be executed when user enters a prompt
 st.text_input(label='.', placeholder='Add a new todo...', key=f'new_todo', on_change=add_todo, label_visibility='hidden')
